15abd6ac59476da94e78bc7dc388293368632789
jaekwanglee <tytut233@gmail.com>
2026-03-16 11:10:12 +0200

    cpp00_ex0

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+#include <iostream> // input/output Library
+#include <cctype> // inc toupper
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    }
+    else
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            for (int j = 0; argv[i][j]; j++)
+            {
+                std::cout << (char)std::toupper(argv[i][j]);
+            }
+        }
+    }
+    std::cout << std::endl;
+    return 0;
+}
+
+//std::endl : change the line('\n')
